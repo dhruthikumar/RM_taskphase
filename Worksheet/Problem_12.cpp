1d9e60342f82bd0fe20c9e85322a17d54096b192
dhruthikumar <96371864+dhruthikumar@users.noreply.github.com>
2021-12-23 14:21:55 +0530

    Add files via upload

---
+++ Worksheet/Problem_12.cpp
@@ -0,0 +1,21 @@
+//Factorial of a number using recursion
+
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    if(n > 1)
+        return n * factorial(n - 1);
+    else
+        return 1;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    cout << "Factorial of " << n << " is: " <<  factorial(n);
+    return 0;
+}
+
