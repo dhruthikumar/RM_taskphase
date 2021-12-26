f4fa82301528fed49958894400e19a1baa4f65b4
dhruthikumar <96371864+dhruthikumar@users.noreply.github.com>
2021-12-26 23:36:31 +0530

    Add files via upload

---
+++ Worksheet/Problem_07.cpp
@@ -0,0 +1,27 @@
+// To copy a string into another string using pointers
+
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+void cpystr(char *, char *);
+int main()
+{
+    char strOrig[100], strCopy[100];
+    cout<<"Enter the string: ";
+    gets(strOrig);
+    cpystr(strOrig, strCopy);
+    cout<<"\nEntered String: "<<strOrig;
+    cout<<"\nCopied String: "<<strCopy;
+    cout<<endl;
+    return 0;
+}
+void cpystr(char *origPtr, char *copPtr)
+{
+    while(*origPtr)
+    {
+        *copPtr = *origPtr;
+        origPtr++;
+        copPtr++;
+    }
+    *copPtr = '\0';
+}
